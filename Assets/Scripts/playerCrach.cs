faae1405de9ac9af9cfcc23d5aec327b8513a837
IslamGad <islam.gad111@gmail.com>
2019-04-22 22:22:25 +0200

    the last destroy
    "the last destroy"

--- Assets/Scripts/playerCrach.cs
+++ Assets/Scripts/playerCrach.cs
@@ -6,16 +6,17 @@ public class playerCrach : MonoBehaviour
 {
     public playerCrach movement;
       Destroy x = new Destroy();
-
+    int counter = 1;
     // This function runs when we hit another object.
     // We get information about the collision and call it "collisionInfo".
     void OnCollisionEnter(Collision collisionInfo)
     {
         // We check if the object we collided with has a tag called "Obstacle".
-        if (collisionInfo.collider.name == "Cube")
+        if (collisionInfo.collider.name == "Cube" && counter ==1)
         {
             
              x.explode(collisionInfo.collider.gameObject);
+            counter++;
          }
     }
 

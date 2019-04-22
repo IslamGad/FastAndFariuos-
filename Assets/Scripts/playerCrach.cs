6b8a6e90d3a03189364e78cdfc56fa33d28913e8
IslamGad <islam.gad111@gmail.com>
2019-04-22 22:28:29 +0200

    22vv

--- Assets/Scripts/playerCrach.cs
+++ Assets/Scripts/playerCrach.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 public class playerCrach : MonoBehaviour
 {
     public playerCrach movement;
-      Destroy x = new Destroy();
+    Destroy x = new Destroy();
     int counter = 1;
     // This function runs when we hit another object.
     // We get information about the collision and call it "collisionInfo".
     void OnCollisionEnter(Collision collisionInfo)
     {
         // We check if the object we collided with has a tag called "Obstacle".
-        if (collisionInfo.collider.name == "Cube" && counter ==1)
-        { 
-            
-             x.explode(collisionInfo.collider.gameObject);
+        if (collisionInfo.collider.name == "Cube" && counter == 1)
+        {
+
+            x.explode(collisionInfo.collider.gameObject);
             counter++;
-         }
+        }
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+s
